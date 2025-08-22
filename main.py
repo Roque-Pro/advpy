3a951eeb92773be8ad5452a39cd3ef5dc6db6eeb
Roque-Pro <roque.rafael.audaces@gmail.com>
2025-08-21 23:13:57 -0300

    Ajuste para rodar Flask corretamente no Render (uso do Gunicorn e PORT)

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from config import create_app
 from auth import auth_bp
 from demandas import demandas_bp
@@ -12,7 +13,7 @@ app.register_blueprint(bp_noticias)
 # app.register_blueprint(noticias_bp)
 
 if __name__ == '__main__':
-    with app.app_context():
-        from extensions import db
-        db.create_all()
-    app.run(debug=True)
+    # Pega a variável de ambiente 'PORT', que o Render define automaticamente
+    port = int(os.environ.get('PORT', 5000))  # Se não encontrar, usa a porta 5000
+    # Executa o Flask na porta correta e com o host 0.0.0.0 para ser acessível externamente
+    app.run(debug=True, host='0.0.0.0', port=port)
